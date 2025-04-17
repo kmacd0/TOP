e1c8fed71e8d525937bd09f7f6bd9606a4858883
Brindobob <102769633+Brindobob@users.noreply.github.com>
2025-04-17 13:38:59 -0400

    created sumAll function

--- JS Foundations/Basics/javascript3.js
+++ JS Foundations/Basics/javascript3.js
@@ -24,15 +24,35 @@ function reverseString(str) {
 
 // console.log(reverseString("hello"));
 
-// Exercise 04 - Write a function removeFromArray which takes an array and args, and removes the args
+/** Exercise 04 - Write a function removeFromArray which takes an array and other arguments
+ * then remove the other arguments from that array
+ */
 
 function removeFromArray(array, item) {
   let newArray = [...array];
-  const index = newArray.findIndex((element) => element === item);
+  const index = newArray.findIndex(function (element) {
+    console.log(element);
+    return element === item;
+  });
   if (index !== -1) {
     newArray.splice(index, 1);
     return newArray;
   }
 }
 
-console.log(removeFromArray([1, 2, 3, 4, 5, 6], 3));
+// console.log(removeFromArray([1, 2, 3, 4, 5, 6], 3));
+
+/** Exercise 05 - Write a function sumAll which takes 2 positive integers and returns the sum of
+ * every integer between (and including) them
+ */
+
+function sumAll(num1, num2) {
+  let steps = [];
+  for (i = num1; i <= num2; i++) {
+    steps.push(i);
+  }
+  steps = steps.reduce((a, b) => a + b, 0);
+  return steps;
+}
+
+console.log(sumAll(1, 8));
