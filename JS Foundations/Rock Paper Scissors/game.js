273435802ab2741adcb20bfaa08eeb524e614194
Brindobob <102769633+Brindobob@users.noreply.github.com>
2025-04-19 16:45:16 -0400

    full rock paper scissors basic interface

--- JS Foundations/Rock Paper Scissors/game.js
+++ JS Foundations/Rock Paper Scissors/game.js
@@ -1,14 +1,80 @@
 const chooseRock = document.querySelector("#rock");
 chooseRock.addEventListener("click", () => {
-    alert("You chose rock.");
+    humanChoice = "rock";
+    playGame();
 });
 
 const choosePaper = document.querySelector("#paper");
 choosePaper.addEventListener("click", () => {
-    alert("You chose paper.");
+    humanChoice = "paper";
+    playGame();
 });
 
 const chooseScissors = document.querySelector("#scissors");
 chooseScissors.addEventListener("click", () => {
-    alert("You chose scissors.");
+    humanChoice = "scissors";
+    playGame();
 });
+
+function getComputerChoice() {
+    const v = Math.floor(Math.random() * 3);
+    if (v === 0) return "rock";
+    else if (v === 1) return "paper";
+    return "scissors";
+}
+
+const humanScoreText = document.querySelector("#humanScore");
+const computerScoreText = document.querySelector("#computerScore");
+const roundNumber = document.querySelector("#roundNumber");
+const roundResult = document.querySelector("#roundResult");
+
+let humanScore = 0;
+let computerScore = 0;
+let gameRound = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return (roundResult.textContent = `You chose ${humanChoice} against ${computerChoice}. Draw.`);
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return (
+            (roundResult.textContent = `You chose ${humanChoice} against ${computerChoice}. You win.`),
+            humanScore++,
+            (humanScoreText.textContent = `Your score: ${humanScore}`)
+        );
+    } else {
+        return (
+            (roundResult.textContent = `You chose ${humanChoice} against ${computerChoice}. Computer wins.`),
+            computerScore++,
+            (computerScoreText.textContent = `Computer score: ${computerScore}`)
+        );
+    }
+}
+
+function playGame() {
+    gameRound++;
+    roundNumber.textContent = `Round: ${gameRound}`;
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    if (gameRound === 10) {
+        endGame();
+    }
+}
+
+function endGame() {
+    document.querySelector("#rock").disabled = true;
+    document.querySelector("#paper").disabled = true;
+    document.querySelector("#scissors").disabled = true;
+
+    if (humanScore > computerScore) {
+        roundResult.textContent = "Congratulations, you won!";
+    } else if (humanScore < computerScore) {
+        roundResult.textContent = "Computer wins.";
+    } else if ((humanScore = computerScore)) {
+        roundResult.textContent = "Game draw.";
+    }
+}
