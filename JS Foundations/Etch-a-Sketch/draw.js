8851149503363d4d40fd012f8b940265726c6a8d
Brindobob <102769633+Brindobob@users.noreply.github.com>
2025-04-20 14:44:11 -0400

    added: grid size, color selection, reset grid

--- JS Foundations/Etch-a-Sketch/draw.js
+++ JS Foundations/Etch-a-Sketch/draw.js
@@ -1,8 +1,37 @@
-function createTiles(size) {
-  const containerDiv = document.querySelector("#container");
-  const tileSize = 700 / size;
+let currentColor = "dodgerblue";
 
-  for (let i = 0; i < size * size; i++) {
+const redBtn = document.querySelector("#redBtn");
+const greenBtn = document.querySelector("#greenBtn");
+const blueBtn = document.querySelector("#blueBtn");
+const yellowBtn = document.querySelector("#yellowBtn");
+const clearGridBtn = document.querySelector("#clearGridBtn");
+
+redBtn.addEventListener("click", () => {
+  currentColor = "red";
+});
+
+greenBtn.addEventListener("click", () => {
+  currentColor = "green";
+});
+
+blueBtn.addEventListener("click", () => {
+  currentColor = "blue";
+});
+
+yellowBtn.addEventListener("click", () => {
+  currentColor = "yellow";
+});
+
+clearGridBtn.addEventListener("click", () => {
+  const containerDiv = document.querySelector("#container");
+  containerDiv.innerHTML = "";
+});
+
+function createTiles(gridSize) {
+  const containerDiv = document.querySelector("#container");
+  const tileSize = 700 / gridSize;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const tileDiv = document.createElement("div");
     tileDiv.classList.add("tile");
     tileDiv.style.width = `${tileSize}px`;
@@ -15,8 +44,25 @@ function createTiles(size) {
       if (currentOpacity < 1) {
         tileDiv.style.opacity = (currentOpacity + 0.1).toString();
       }
-      tileDiv.style.backgroundColor = "dodgerblue";
+      tileDiv.style.backgroundColor = currentColor;
     });
   }
 }
+
+const gridSizeBtn = document.querySelector("#gridSizeBtn");
+gridSizeBtn.addEventListener("click", () => {
+  const input = prompt("Enter a grid size ranging from 5-100.");
+  const gridSize = parseInt(input, 10);
+
+  if (isNaN(gridSize) || gridSize < 5 || gridSize > 100) {
+    alert("Enter a valid number between 5-100.");
+    return;
+  }
+
+  const containerDiv = document.querySelector("#container");
+  containerDiv.innerHTML = "";
+
+  createTiles(gridSize);
+});
+
 createTiles(10);
